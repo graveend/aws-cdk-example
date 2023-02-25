af4a22ba869bbd9662e27d89a63892bd034ca0ff
graveend <geethu.raveendran2110@gmail.com>
2023-02-25 19:28:25 +0000

    Specify account

--- bin/aws-cdk-example.ts
+++ bin/aws-cdk-example.ts
@@ -15,7 +15,7 @@ new AwsCdkExampleStack(app, 'AwsCdkExampleStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '521013699958', region: 'eu-west-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
